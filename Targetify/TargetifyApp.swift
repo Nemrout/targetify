293d0e5ec7ac88478b7d24ce715801b2cda4aaf5
Petros Tepoyan <petros@tepoyan.ru>
2022-10-03 00:38:50 +0400

    Initial Commit

---
+++ Targetify/TargetifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TargetifyApp.swift
+//  Targetify
+//
+//  Created by Петрос Тепоян on 10/3/22.
+//
+
+import SwiftUI
+
+@main
+struct TargetifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
